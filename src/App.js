97ea79fa1c61ee162da6bd8373cc18912c4c296e
Samuel Candia <scandia022@gmail.com>
2023-12-27 15:27:44 -0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Advertisement from './components/Advertisement/Advertisement';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Navigation from './components/Navigation/Navigation';
+import SubContents from './components/SubContents/SubContents';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Navigation />
+      <Main>
+        <SubContents />
+        <SubContents />
+        <SubContents />
+        <Advertisement />
+      </Main>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import styles from "./Navigation.module.css";
+
+
+class Navigation extends Component {
+
+
+    render() {
+
+        return (
+            <div className={styles.navigation}></div>
+        )
+    }
+}
+
+export default Navigation;
